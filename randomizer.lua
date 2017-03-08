a84b02fceb2e62ac4efb3692ea56e9730a504f63
Sebastian Gabl <sebastian.gabl@gmx.at>
2017-03-08 20:44:44 +0100

    Update randomizer
    Added options to randomize the rest of the players equipment, improved
    visuals in the loadout screen to show equipment as locked when it was
    randomized and added option to hide randomized equipment.

--- randomizer.lua
+++ randomizer.lua
@@ -20,47 +20,43 @@ function Randomizer:load()
 end
 
 function Randomizer:allow_randomizing()
-  local in_heist = managers and managers.player and managers.player:player_unit()
-  return in_heist
+  return Utils:IsInGameState()
+end
+
+function Randomizer:is_randomized(selection)
+  local mapping = {
+    [1] = self.data.random_primary,
+    [2] = self.data.random_secondary,
+    [3] = self.data.random_melee,
+    [4] = self.data.random_grenade,
+    [5] = self.data.random_armor,
+    [6] = self.data.random_deployable
+  }
+  return mapping[selection]
+end
+
+function Randomizer:get_loadout_item_index()
+  self._loadout_item_index = self._loadout_item_index or 0
+  self._loadout_item_index = self._loadout_item_index + 1
+  return self._loadout_item_index
 end
 
 function Randomizer:chk_setup_weapons()
   if not self.weapons then
     self.weapons = {}
     for weapon, data in pairs(tweak_data.weapon) do
-      local owned = not data.global_value or managers.dlc:is_dlc_unlocked(data.global_value)
-      if data.autohit and owned then
-        local selection_index = data.use_data.selection_index
-        self.weapons[selection_index] = self.weapons[selection_index] or {}
-        local data = {
-          selection_index = selection_index,
-          weapon_id = weapon,
-          factory_id = managers.weapon_factory:get_factory_id_by_weapon_id(weapon)
-        }
-        table.insert(self.weapons[selection_index], data)
-      end
-    end
-  end
-end
-
-function Randomizer:chk_setup_grenades()
-  if not self.grenades then
-    self.grenades = {}
-    for grenade_id, grenade in pairs(tweak_data.blackmarket.projectiles) do
-      local owned = not grenade.dlc or managers.dlc:is_dlc_unlocked(grenade.dlc)
-      if grenade.throwable and owned then
-        table.insert(self.grenades, { id = grenade_id, amount = grenade.max_amount })
-      end
-    end
-  end
-end
-
-function Randomizer:chk_setup_melees()
-  if not self.melees then
-    self.melees = {}
-    for melee_weapon, data in pairs(tweak_data.blackmarket.melee_weapons) do
-      if not data.dlc or managers.dlc:is_dlc_unlocked(data.dlc) then
-        table.insert(self.melees, melee_weapon)
+      if data.autohit then
+        local unlocked = Global.blackmarket_manager.weapons[weapon].unlocked and (not data.global_value or managers.dlc:is_dlc_unlocked(data.global_value))
+        if unlocked then
+          local selection_index = data.use_data.selection_index
+          self.weapons[selection_index] = self.weapons[selection_index] or {}
+          local data = {
+            selection_index = selection_index,
+            weapon_id = weapon,
+            factory_id = managers.weapon_factory:get_factory_id_by_weapon_id(weapon)
+          }
+          table.insert(self.weapons[selection_index], data)
+        end
       end
     end
   end
@@ -101,10 +97,36 @@ function Randomizer:get_random_weapon(selection_index)
   return self._random_weapon[selection_index]
 end
 
+function Randomizer:chk_setup_grenades()
+  if not self.grenades then
+    self.grenades = {}
+    for grenade_id, data in pairs(tweak_data.blackmarket.projectiles) do
+      if data.throwable or data.ability then
+        local unlocked = Global.blackmarket_manager.grenades[grenade_id].unlocked and (not data.dlc or managers.dlc:is_dlc_unlocked(data.dlc))
+        if unlocked then
+          table.insert(self.grenades, { id = grenade_id, amount = data.max_amount })
+        end
+      end
+    end
+  end
+end
+
 function Randomizer:get_random_grenade()
   self:chk_setup_grenades()
   self._random_grenade = self._random_grenade or self.grenades[math.random(#self.grenades)]
   return self._random_grenade.id, self._random_grenade.amount
+end
+
+function Randomizer:chk_setup_melees()
+  if not self.melees then
+    self.melees = {}
+    for melee_weapon, data in pairs(tweak_data.blackmarket.melee_weapons) do
+      local unlocked = Global.blackmarket_manager.melee_weapons[melee_weapon].unlocked and (not data.dlc or managers.dlc:is_dlc_unlocked(data.dlc))
+      if unlocked then
+        table.insert(self.melees, melee_weapon)
+      end
+    end
+  end
 end
 
 function Randomizer:get_random_melee()
@@ -113,48 +135,171 @@ function Randomizer:get_random_melee()
   return self._random_melee
 end
 
+function Randomizer:chk_setup_armors()
+  if not self.armors then
+    self.armors = {}
+    for armor, _ in pairs(tweak_data.blackmarket.armors) do
+      local unlocked = Global.blackmarket_manager.armors[armor].unlocked
+      if unlocked then
+        table.insert(self.armors, armor)
+      end
+    end
+  end
+end
+
+function Randomizer:get_random_armor()
+  self:chk_setup_armors()
+  self._random_armor = self._random_armor or self.armors[math.random(#self.armors)]
+  return self._random_armor
+end
+
+function Randomizer:chk_setup_deployables()
+  if not self.deployables then
+    self.deployables = {}
+    for deployable, data in pairs(tweak_data.equipments) do
+      if data.visual_object then
+        table.insert(self.deployables, deployable)
+      end
+    end
+  end
+end
+
+function Randomizer:get_random_deployable()
+  self:chk_setup_deployables()
+  self._random_deployable = self._random_deployable or self.deployables[math.random(#self.deployables)]
+  return self._random_deployable
+end
+
 ------------------------ MOD STUFF ------------------------
 if RequiredScript == "lib/managers/blackmarketmanager" then
   
-  local equipped_primary_original = BlackMarketManager.equipped_primary
-  function BlackMarketManager:equipped_primary(...)
+  local forced_primary_original = BlackMarketManager.forced_primary
+  function BlackMarketManager:forced_primary(...)
     if not Randomizer.data.random_primary or not Randomizer:allow_randomizing() then
-      return equipped_primary_original(self, ...)
+      return forced_primary_original(self, ...)
     end
     return Randomizer:get_random_weapon(2)
   end
 
-  local equipped_secondary_original = BlackMarketManager.equipped_secondary
-  function BlackMarketManager:equipped_secondary(...)
+  local forced_secondary_original = BlackMarketManager.forced_secondary
+  function BlackMarketManager:forced_secondary(...)
     if not Randomizer.data.random_secondary or not Randomizer:allow_randomizing() then
-      return equipped_secondary_original(self, ...)
+      return forced_secondary_original(self, ...)
     end
     return Randomizer:get_random_weapon(1)
   end
 
-  local equipped_grenade_original = BlackMarketManager.equipped_grenade
-  function BlackMarketManager:equipped_grenade(...)
-    --if not Randomizer.data.random_grenade or not Randomizer:allow_randomizing() then
-      return equipped_grenade_original(self, ...)
-    --end
-    --self._original_grenade = self._original_grenade or equipped_grenade_original(self, ...)
-    --return Randomizer:get_random_grenade()
+  local forced_throwable_original = BlackMarketManager.forced_throwable
+  function BlackMarketManager:forced_throwable(...)
+    if not Randomizer.data.random_grenade or not Randomizer:allow_randomizing() then
+      return forced_throwable_original(self, ...)
+    end
+    return Randomizer:get_random_grenade()
   end
 
   local equipped_melee_weapon_original = BlackMarketManager.equipped_melee_weapon
   function BlackMarketManager:equipped_melee_weapon(...)
-    --if not Randomizer.data.random_melee or not Randomizer:allow_randomizing() then
-      return equipped_melee_weapon_original(self, ...)
-    --end
-    --self._original_melee = self._original_melee or equipped_melee_weapon_original(self, ...)
-    --return Randomizer:get_random_melee()
+    local forced_melee_weapon = self:forced_melee_weapon()
+    if forced_melee_weapon then
+      return forced_melee_weapon
+    end
+    return equipped_melee_weapon_original(self, ...)
   end
   
-  local save_original = BlackMarketManager.save
-  function BlackMarketManager:save(data)
-    save_original(self, data)
-    data.blackmarket.equipped_grenade = self._original_grenade or data.blackmarket.equipped_grenade
-    data.blackmarket.equipped_melee_weapon = self._original_melee or data.blackmarket.equipped_melee_weapon
+  function BlackMarketManager:forced_melee_weapon(...)
+    if not Randomizer.data.random_melee or not Randomizer:allow_randomizing() then
+      return
+    end
+    return Randomizer:get_random_melee()
+  end
+  
+  local forced_armor_original = BlackMarketManager.forced_armor
+  function BlackMarketManager:forced_armor(...)
+    if not Randomizer.data.random_armor or not Randomizer:allow_randomizing() then
+      return forced_armor_original(self, ...)
+    end
+    return Randomizer:get_random_armor()
+  end
+  
+  local forced_deployable_original = BlackMarketManager.forced_deployable
+  function BlackMarketManager:forced_deployable(...)
+    if not Randomizer.data.random_deployable or not Randomizer:allow_randomizing() then
+      return forced_deployable_original(self, ...)
+    end
+    return Randomizer:get_random_deployable()
+  end
+  
+end
+
+if RequiredScript == "lib/managers/menu/missionbriefinggui" then
+
+  local init_loadout_tab_original = NewLoadoutTab.init
+  function NewLoadoutTab:init(...)
+    init_loadout_tab_original(self, ...)
+    Randomizer._loadout_item_index = 0
+  end
+
+  local init_loadout_item_original = NewLoadoutItem.init
+  function NewLoadoutItem:init(...)
+    init_loadout_item_original(self, ...)
+    if Randomizer:is_randomized(Randomizer:get_loadout_item_index()) then
+      if Randomizer.data.hide_selections then
+        if self._info_icon_panel then
+          self._info_icon_panel:set_alpha(0)
+        end
+        self._info_text:set_alpha(0)
+        local questionmark = self._item_panel:text({
+          name = "questionmark",
+          text = "?",
+          font = tweak_data.menu.eroded_font,
+          font_size = 140,
+          align = "center",
+          vertical = "center",
+          color = Color.black
+        })
+        questionmark:set_center(self._item_panel:center_x(), self._item_panel:center_y())
+      end
+      if self._item_image then
+        self._item_image:set_color(Color.black:with_alpha(Randomizer.data.hide_selections and 0 or 1))
+      end
+      if self._item_image1 then
+        self._item_image1:set_color(Color.black:with_alpha(Randomizer.data.hide_selections and 0 or 1))
+      end
+      if self._item_image2 then
+        self._item_image2:set_color(Color.black:with_alpha(Randomizer.data.hide_selections and 0 or 1))
+      end
+      local lock = self._item_panel:bitmap({
+        name = "lock",
+        texture = "guis/textures/pd2/skilltree/padlock",
+        w = 32,
+        h = 32,
+        color = tweak_data.screen_colors.text,
+        layer = 2
+      })
+      lock:set_center(self._item_panel:center_x(), self._item_panel:center_y())
+    end
+  end
+  
+  local set_slot_outfit_original = TeamLoadoutItem.set_slot_outfit
+  function TeamLoadoutItem:set_slot_outfit(slot, criminal_name, outfit, ...)
+    if slot ~= 1 or not Randomizer.data.hide_selections or not outfit then
+      return set_slot_outfit_original(self, slot, criminal_name, outfit, ...)
+    end
+    outfit.primary.factory_id = not Randomizer.data.random_primary and outfit.primary.factory_id
+    outfit.secondary.factory_id = not Randomizer.data.random_secondary and outfit.secondary.factory_id
+    outfit.melee_weapon = not Randomizer.data.random_melee and outfit.melee_weapon
+    outfit.grenade = not Randomizer.data.random_grenade and outfit.grenade
+    outfit.armor = not Randomizer.data.random_armor and outfit.armor
+    outfit.deployable = not Randomizer.data.random_deployable and outfit.deployable
+    return set_slot_outfit_original(self, slot, criminal_name, outfit, ...)
+  end
+  
+  local confirm_pressed_original = NewLoadoutTab.confirm_pressed
+  function NewLoadoutTab:confirm_pressed(...)
+    if Randomizer:is_randomized(self._item_selected) then
+      return
+    end
+    return confirm_pressed_original(self, ...)
   end
   
 end
@@ -188,13 +333,28 @@ if RequiredScript == "lib/managers/menumanager" then
     end
 
     MenuHelper:AddToggle({
+      id = "hide_selections",
+      title = "hide_selections_name",
+      desc = "hide_selections_desc",
+      callback = "Randomizer_toggle",
+      value = Randomizer.data.hide_selections,
+      menu_id = menu_id_main,
+      priority = 11
+    })
+    MenuHelper:AddDivider({
+      id = "divider",
+      size = 24,
+      menu_id = menu_id_main,
+      priority = 10
+    })
+    MenuHelper:AddToggle({
       id = "random_primary",
       title = "bm_menu_primaries",
       desc = "primary_desc",
       callback = "Randomizer_toggle",
       value = Randomizer.data.random_primary,
       menu_id = menu_id_main,
-      priority = 100
+      priority = 6
     })
     MenuHelper:AddToggle({
       id = "random_secondary",
@@ -203,7 +363,7 @@ if RequiredScript == "lib/managers/menumanager" then
       callback = "Randomizer_toggle",
       value = Randomizer.data.random_secondary,
       menu_id = menu_id_main,
-      priority = 99
+      priority = 5
     })
     MenuHelper:AddToggle({
       id = "random_melee",
@@ -212,8 +372,7 @@ if RequiredScript == "lib/managers/menumanager" then
       callback = "Randomizer_toggle",
       value = Randomizer.data.random_melee,
       menu_id = menu_id_main,
-      disabled = true,
-      priority = 98
+      priority = 4
     })
     MenuHelper:AddToggle({
       id = "random_grenade",
@@ -222,8 +381,25 @@ if RequiredScript == "lib/managers/menumanager" then
       callback = "Randomizer_toggle",
       value = Randomizer.data.random_grenade,
       menu_id = menu_id_main,
-      disabled = true,
-      priority = 97
+      priority = 3
+    })
+    MenuHelper:AddToggle({
+      id = "random_armor",
+      title = "bm_menu_armor",
+      desc = "armor_desc",
+      callback = "Randomizer_toggle",
+      value = Randomizer.data.random_armor,
+      menu_id = menu_id_main,
+      priority = 2
+    })
+    MenuHelper:AddToggle({
+      id = "random_deployable",
+      title = "bm_menu_deployables",
+      desc = "deployable_desc",
+      callback = "Randomizer_toggle",
+      value = Randomizer.data.random_deployable,
+      menu_id = menu_id_main,
+      priority = 1
     })
     
   end)
