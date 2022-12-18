455d7d1598c1ec53b84822576520f877cb09c98b
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-12-18 18:28:54 +0100

    Fix potential crash when setting gadget colors

--- mod.lua
+++ mod.lua
@@ -426,29 +426,30 @@ elseif RequiredScript == "lib/units/weapons/newraycastweaponbase" then
 			return
 		end
 
-		for _, part_id in ipairs(self._blueprint) do
+		local function try_set_color(part_id)
 			NewRaycastWeaponBase.GADGET_COLORS[part_id] = NewRaycastWeaponBase.GADGET_COLORS[part_id] or {
 				laser = Color(hsv_to_rgb(math.random(360), math.random() * 0.5 + 0.5, math.random() * 0.25 + 0.75)),
 				flashlight = Color(1, 1, math.random() * 0.3 + 0.7)
 			}
-			local colors = NewRaycastWeaponBase.GADGET_COLORS[part_id]
 
 			local mod_td = tweak_data.weapon.factory.parts[part_id]
 			local part_data = self._parts[part_id]
+			local colors = NewRaycastWeaponBase.GADGET_COLORS[part_id]
 
-			if colors[mod_td.sub_type] then
+			if part_data and colors[mod_td.sub_type] then
 				local alpha = part_data.unit:base().GADGET_TYPE == "laser" and tweak_data.custom_colors.defaults.laser_alpha or 1
 				part_data.unit:base():set_color(colors[mod_td.sub_type]:with_alpha(alpha))
 			end
 
 			if mod_td.adds then
 				for _, add_part_id in ipairs(mod_td.adds) do
-					if self._parts[add_part_id] and self._parts[add_part_id].unit:base() then
-						local sub_type = tweak_data.weapon.factory.parts[add_part_id].sub_type
-						self._parts[add_part_id].unit:base():set_color(colors[sub_type])
-					end
+					try_set_color(add_part_id)
 				end
 			end
+		end
+
+		for _, part_id in ipairs(self._blueprint) do
+			try_set_color(part_id)
 		end
 	end)
 
